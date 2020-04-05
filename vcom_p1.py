ca6d692a90b3a98521f1532de052da0b9f6e034c
Cesar-Medeiros <cesar.mnm@gmail.com>
2020-04-05 17:01:56 +0100

    Add color label Improve contour detection Remove small noise

--- vcom_p1.py
+++ vcom_p1.py
@@ -13,8 +13,6 @@ from __future__ import print_function
 import numpy as np
 import cv2 as cv
 
-font = cv.FONT_HERSHEY_COMPLEX
-
 IMAGES_DIR = 'images/'
 IMAGES_SIMPLE_DIR = IMAGES_DIR + 'simple/'
 IMAGES_COMPLEX_DIR = IMAGES_DIR + 'complex/'
@@ -39,30 +37,27 @@ def capture_frame():
 
 def hsvRedSegmentation(img):
     hsv = cv.cvtColor(img,cv.COLOR_BGR2HSV)
+
     # first range of red
-    lower_red = np.array([0,130,80])
+    lower_red = np.array([0,200,50])
     upper_red = np.array([5,255,255])
     mask1 = cv.inRange(hsv,lower_red,upper_red)
+
     # second range of red
-    lower_red = np.array([170,130,80])
+    lower_red = np.array([170,200,50])
     upper_red = np.array([180,255,255])
     mask2 = cv.inRange(hsv,lower_red,upper_red)
+
     # combining masks
     mask = mask1 + mask2
-    maskN = cv.morphologyEx(mask, cv.MORPH_OPEN, np.ones((3,3),np.uint8))
-    maskN = cv.morphologyEx(mask, cv.MORPH_DILATE, np.ones((3,3),np.uint8))
-    return maskN
+    return mask
 
 def hsvBlueSegmentation(img):
     hsv = cv.cvtColor(img,cv.COLOR_BGR2HSV)
-
     lower_blue = np.array([104,200,100])
-    upper_blue = np.array([110,255,255])
+    upper_blue = np.array([115,255,255])
     mask = cv.inRange(hsv,lower_blue,upper_blue)
-
-    maskN = cv.morphologyEx(mask, cv.MORPH_OPEN, np.ones((3,3),np.uint8))
-    maskN = cv.morphologyEx(mask, cv.MORPH_DILATE, np.ones((3,3),np.uint8))
-    return maskN
+    return mask
 
 def getEdges(gray):
     return cv.Canny(gray,threshold1=50,threshold2=200)
@@ -72,9 +67,10 @@ def getContours(edges):
     return contours
 
 def getApproxContour(contour):
-        epsilon = 0.04*cv.arcLength(cnt,True)
-        approx = cv.approxPolyDP(cnt, epsilon, True)
-        return approx
+    # between 1% and 5%
+    epsilon = 0.04*cv.arcLength(cnt,True)
+    approx = cv.approxPolyDP(cnt, epsilon, True)
+    return approx
 
 def getShapeName(numVertices):
     text = ""
@@ -89,46 +85,107 @@ def getShapeName(numVertices):
 
     return text
 
+def saturation_eq(img):
+    hsv = cv.cvtColor(img,cv.COLOR_BGR2HSV)
+    h, s, v = cv.split(hsv)
+    new_s = cv.equalizeHist(s)
+    hsv_eq = cv.merge([h, new_s, v])
+    bgr_eq = cv.cvtColor(hsv_eq,cv.COLOR_HSV2BGR)
+    return bgr_eq
+
+def increase_sat(img, alpha, beta):
+    hsv = cv.cvtColor(img,cv.COLOR_BGR2HSV)
+    h, s, v = cv.split(hsv)
+    new_s = cv.convertScaleAbs(s, alpha=alpha, beta=beta)
+    hsv_eq = cv.merge([h, new_s, v])
+    bgr_eq = cv.cvtColor(hsv_eq,cv.COLOR_HSV2BGR)
+    return bgr_eq
+
+def test(img1):
+    # Create zeros array to store the stretched image
+    minmax_img = np.zeros((img1.shape[0],img1.shape[1]),dtype = 'uint8')
+    
+    # Loop over the image and apply Min-Max formulae
+    for i in range(img1.shape[0]):
+        for j in range(img1.shape[1]):
+            minmax_img[i,j] = 255*(img1[i,j]-np.min(img1))/(np.max(img1)-np.min(img1))
+    
+    return minmax_img
+
+
 if __name__ == '__main__':
     print(__doc__)
 
     # load image
     # img = capture_frame()
-    img = load_image(IMAGES_SIMPLE_DIR + '1.jpg')
+    img = load_image(IMAGES_COMPLEX_DIR + '1.jpg')
+    cv.imshow("Img", img)
+
+    # Smooth image maintaing edges
+    img = cv.bilateralFilter(img,10,30,30)
+
+    # Remove small noise from edges left from bilateral filter
+    img = cv.GaussianBlur(img, (3,3), 0)
+
+    #Equalize saturation
+    # img_eq = increase_sat(img, 1, 20)
+    img_eq = saturation_eq(img)
+
+    cv.imshow('ImgEq',img_eq)
+   
 
 
-    # hsv red segmentation
-    mask1 = hsvRedSegmentation(img)
-    mask2 = hsvBlueSegmentation(img)
+    # hsv segmentation
+    masks = [
+                hsvRedSegmentation(img_eq),
+                hsvBlueSegmentation(img_eq)
+            ]
+    name = [
+        "red",
+        "blue"
+    ]
+
+    for i, mask in enumerate(masks):
+
+        # Remove small noise
+        mask = cv.medianBlur(mask, 3)
+
+        # Close mask gaps
+        kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))
+        mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, kernel)
+
+        cv.imshow('Mask' + str(i),mask)
+
+
+    # Edge canvas
+    height, width, channels = img.shape 
+    edge_canvas = np.zeros((height,width,1), np.uint8)
+
+    for i, mask in enumerate(masks):
+
+        # find countours
+        contours = getContours(mask)
+
+        for cnt in contours:
+            # close open contours
+            hull = cv.convexHull(cnt)
+
+            # discard noise
+            if cv.contourArea(hull) < 100:
+                cv.fillPoly(mask, pts =[cnt], color=(0,0,0))
+                continue
+
+            approx = getApproxContour(cnt)
+            x = approx.ravel()[0]
+            y = approx.ravel()[1]
+            shapeName = getShapeName(len(approx))
+
+            cv.drawContours(edge_canvas, [cnt], -1, (255, 255, 255), 1)
+            cv.drawContours(img, [approx], -1, (0, 255, 0), 2)
+            cv.putText(img, name[i] + ' ' + shapeName + ' ' + str(len(approx)), (x, y), cv.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), 2)
     
-    gray = mask1 + mask2
-    blurred = cv.GaussianBlur(gray, (5, 5), 0)
-    thresh = cv.threshold(blurred, 60, 255, cv.THRESH_BINARY)[1]
 
-    # canny edge detection
-    edges = getEdges(thresh)
-    cv.imshow('Edges',edges)
-
-    # find countours
-    contours = getContours(edges)
-
-    contoursImage = img.copy()
-    cv.drawContours(contoursImage, contours, -1, (0, 255, 255), 2)
-
-    # identify shape
-    for cnt in contours:
-        if cv.contourArea(cnt) < 100:
-            continue
-        approx = getApproxContour(cnt)
-        x = approx.ravel()[0]
-        y = approx.ravel()[1]
-        shapeName = getShapeName(len(approx))
-        cv.drawContours(contoursImage, [approx], -1, (0, 255, 0), 2)
-        cv.putText(contoursImage, shapeName + ' ' + str(len(approx)), (x, y), cv.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), 2)
-
-    # draw countours
-    cv.imshow('Countours',contoursImage)
-
-    cv.imshow('Image', img)
+    cv.imshow('Edges',edge_canvas)
+    cv.imshow('Final', img)
     cv.waitKey(0)
     cv.destroyAllWindows()
